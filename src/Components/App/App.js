df6d3bb2ffd2f410161e2b9b471ddb3b9ccdf207
Ruy Mallo <ruymallo@gmail.com>
2018-03-24 20:01:29 -0300

    initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import 'Styles/normalize.css'
+import 'Styles/App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div id="App" className="App">
+				this is the App Component
+			</div>
+		);
+	}
+}
+
+export default App;
